a896f4b2ef641781fe11c3013015f0077f919ff4
yarrou <konic2884@gmail.com>
2020-08-31 06:11:37 +0300

    added DBManager

---
+++ src/main/java/org/change/shop/DBManager.java
@@ -0,0 +1,25 @@
+package org.change.shop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    private Connection conn;
+    private  String url;
+    private String user;
+    private  String password;
+
+    public DBManager(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection to the server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
