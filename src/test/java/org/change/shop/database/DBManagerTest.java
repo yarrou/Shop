9b321777b56f45de65db6d4ccbedd3561cfec14a
yarrou <konic2884@gmail.com>
2020-09-01 19:17:56 +0300

    added test DBManagerTest

---
+++ src/test/java/org/change/shop/database/DBManagerTest.java
@@ -0,0 +1,22 @@
+package org.change.shop.database;
+
+import org.change.shop.database.models.Customer;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class DBManagerTest {
+    DBManager dbManager ;
+
+    @org.junit.jupiter.api.Test
+    @Test
+    public void getCustomerTest() throws SQLException {
+        int testId = 3;
+        dbManager = new DBManager(System.getenv("PG_CONN_STRING"),System.getenv("PG_USR"),System.getenv("PG_PSSWRD"));
+        Customer customer=dbManager.getCustomer(testId);
+        Assert.assertEquals(testId,customer.getId());
+    }
+}
